33ceea5a527c46afd85b4c38c6738f0085a2523b
Omar <omar.villasenor@tps.ai>
2025-05-02 17:34:44 -0600

    Cleaned App.jsx

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,35 @@
-export default function Meals({ children }) {
+import {useEffect} from "react";
+import {useState} from "react";
+import Meal from "./Meal.jsx";
+
+export default function Meals() {
+    const [mealsState, setMealsState] = useState();
+    
+        useEffect(() => {
+            async function getMeals() {
+                const response = await fetch("http://localhost:3000/meals");
+                const meals = await response.json();
+                setMealsState(meals);
+            }
+    
+            getMeals();
+        }, []);
+    
     return (
         <div id="meals">
-            { children }
+            {
+                mealsState && mealsState.map(meal => {
+                    return (
+                        <Meal
+                        key={meal.id}
+                        id={meal.id}
+                        name={meal.name}
+                        price={meal.price}
+                        description={meal.description}
+                        image={meal.image}/>
+                    )
+                })
+            }
         </div>
     )
 }
